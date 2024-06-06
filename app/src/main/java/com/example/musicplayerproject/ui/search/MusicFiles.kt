d0dfdb6d72e9f2ed59aa454f5c6681c0406f8feb
Akmall-rey <raihanakmal354@gmail.com>
2024-06-06 09:29:38 +0700

    revisi bagian search

--- app/src/main/java/com/example/musicplayerproject/ui/search/MusicFiles.kt
+++ app/src/main/java/com/example/musicplayerproject/ui/search/MusicFiles.kt
@@ -1,4 +1,39 @@
 package com.example.musicplayerproject.ui.search
 
 class MusicFiles {
+    private val path: String
+    private val title: String
+    private val artist: String
+    private val album: String
+    private val duration: String
+
+    constructor(path: String, title: String, artist: String, album: String, duration: String) {
+        this.path = path
+        this.title = title
+        this.artist = artist
+        this.album = album
+        this.duration = duration
+    }
+
+    fun getPath(): String {
+        return path;
+    }
+
+    fun getTitle(): String {
+        return title;
+    }
+
+    fun getArtist(): String {
+        return artist;
+    }
+
+    fun getAlbum(): String {
+        return album;
+    }
+
+    fun getDuration(): String {
+        return duration;
+    }
+
+
 }
