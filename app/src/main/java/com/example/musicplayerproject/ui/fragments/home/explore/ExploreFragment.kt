ad6c591428d9f5e30b42da25163c7bc36f7ba533
Retno Pinka <retnopinka20@gmail.com>
2024-06-12 18:38:52 +0700

    revisi fragment explore buat load nama artist

--- app/src/main/java/com/example/musicplayerproject/ui/fragments/home/explore/ExploreFragment.kt
+++ app/src/main/java/com/example/musicplayerproject/ui/fragments/home/explore/ExploreFragment.kt
@@ -4,21 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.musicplayerproject.R
+import com.example.musicplayerproject.network.ApiConfig
 import com.example.musicplayerproject.response.MusicResponse
 import com.example.musicplayerproject.response.TrackItem
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import androidx.recyclerview.widget.RecyclerView
-import com.example.musicplayerproject.network.ApiConfig
 
 class ExploreFragment : Fragment() {
 
     private lateinit var musicAdapter: MusicAdapter
     private lateinit var recyclerView: RecyclerView
+    private lateinit var artistNameTextView: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,6 +29,7 @@ class ExploreFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_explore, container, false)
         recyclerView = view.findViewById(R.id.music_explore_recyclerview)
+        artistNameTextView = view.findViewById(R.id.artist_name)
         return view
     }
 
@@ -50,6 +53,10 @@ class ExploreFragment : Fragment() {
                     val data = response.body()?.track
                     data?.let {
                         musicAdapter.setData(it as ArrayList<TrackItem>)
+                        if (it.isNotEmpty()) {
+                            artistNameTextView.text =
+                                it[0].strArtist // Set the artist name to the TextView
+                        }
                     }
                 }
             }
